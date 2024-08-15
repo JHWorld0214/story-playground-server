6ddad3eff5fb71daef5bda62441c4b31e819325d
Seonho Kim <5061ksh@naver.com>
2024-08-15 18:15:51 +0900

    feat : add configuration file for using CLOVA API

---
+++ src/main/java/com/softgallery/story_playground_server/config/ClovaConfig.java
@@ -0,0 +1,9 @@
+package com.softgallery.story_playground_server.config;
+
+public class ClovaConfig {
+    public static final String BASE_URL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt";
+    public static final String CONTENT_TYPE = "application/octet-stream";
+    public static final String HEADER_ID = "X-NCP-APIGW-API-KEY-ID";
+    public static final String HEADER_KEY = "X-NCP-APIGW-API-KEY";
+    public static final String LANG_CODE = "Kor";
+}
