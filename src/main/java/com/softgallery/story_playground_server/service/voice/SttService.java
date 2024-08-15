6f653651f9a788e34ef5193f21a1e9a1132aacd6
Seonho Kim <5061ksh@naver.com>
2024-08-15 15:08:06 +0900

    feat : add service class template for stt

---
+++ src/main/java/com/softgallery/story_playground_server/service/voice/SttService.java
@@ -0,0 +1,11 @@
+package com.softgallery.story_playground_server.service.voice;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@NoArgsConstructor
+@Service
+@Transactional
+public class SttService {
+}
